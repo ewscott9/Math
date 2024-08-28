64d3aabeebacd3cef8725434cc0c9e57383bd418
U-AD3\ewscott <AD3+ewscott@IET-3VBDGT3.ou.ad3.ucdavis.edu>
2024-08-28 11:43:07 -0700

    edit division test

--- math.c
+++ math.c
@@ -188,7 +188,7 @@ bool test_div_num_den(int a, int b)
     // avoid div by 0.
     if (b == 0) b++;
 
-    printf("%d / %d = \n", a, b);
+    printf("%d / %d\n", a, b);
     printf("quotient = ");
     x = (a / b == get_int());
 
